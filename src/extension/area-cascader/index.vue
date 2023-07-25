d79c393a104b44700fa1cd165f21fdd40d59ed9a
liao文禧 <1219585136@qq.com>
2023-07-25 17:17:44 +0800

    feat: area-cascader

--- src/extension/area-cascader/index.vue
+++ src/extension/area-cascader/index.vue
@@ -1,7 +1,6 @@
 <template>
     <form-item-wrapper :prop-name="propName" :designer="designer" :field="field" :rules="rules" :design-state="designState"
-        :parent-widget="parentWidget" :parent-list="parentList" :index-of-parent-list="indexOfParentList"
-        :sub-form-row-index="subFormRowIndex" :sub-form-col-index="subFormColIndex" :sub-form-row-id="subFormRowId">
+        :parent-widget="parentWidget" :parent-list="parentList" :index-of-parent-list="indexOfParentList">
         <div class="full-width-input">
             <el-cascader ref="fieldEditor" :options="field.options.optionItems" v-model="fieldModel"
                 :disabled="field.options.disabled" :size="widgetSize" :clearable="field.options.clearable"
@@ -41,25 +40,12 @@ export default {
             default: ''
         },
 
-
         designState: {
             type: Boolean,
             default: false
         },
 
-        subFormRowIndex: { /* 子表单组件行索引，从0开始计数 */
-            type: Number,
-            default: -1
-        },
-        subFormColIndex: { /* 子表单组件列索引，从0开始计数 */
-            type: Number,
-            default: -1
-        },
-        subFormRowId: { /* 子表单组件行Id，唯一id且不可变 */
-            type: String,
-            default: ''
-        },
-
+        refName: String
     },
     components: {
         FormItemWrapper,
